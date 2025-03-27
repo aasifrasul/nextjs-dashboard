26c4153545457efe0fdb842731ffef629a06d255
Aasif Rasul <aasifrasul@gmail.com>
2025-03-27 14:56:33 +0530

    Fix bugs

---
+++ app/home/counter/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useState, useReducer } from 'react';
+import { Button } from '@/app/ui/button';
+
+interface Action {
+	type: 'increment' | 'decrement',
+	payload?: number
+}
+
+interface State {
+	count: number,
+}
+
+const initialState: State = {
+	count: 0
+}
+
+const countReducer = (state: State = initialState, action: Action) => {
+	switch (action.type) {
+		case 'increment':
+			return {
+				...state,
+				count: state.count + 1,
+			}
+		case 'decrement':
+			return {
+				...state,
+				count: state.count - 1,
+			}
+	}
+}
+
+export default function Page() {
+	const [state, dispatch] = useReducer(countReducer, initialState);
+
+	const handleIncrement = () => dispatch({ type: 'increment' });
+	const handleDecrement = () => dispatch({ type: 'decrement' });
+
+	return (
+		<>
+			<div>Count: {state.count}</div>
+			<div className={'flex flex-direction-row'}>
+				<Button onClick={(e) => handleIncrement()}>+</Button>
+				<Button onClick={(e) => handleDecrement()}>-</Button>
+			</div>
+		</>
+	);
+}
